804e65726873123f625b942409a3cb517e334bd5
Saloxiddin701 <abduraxmon.69list.ru@gmail.com>
2022-03-09 12:46:35 +0500

    progress-bar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,79 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <main>
+      <div class="navigation">
+        <header>
+          <h1>Sayfuddinov Saloxiddin</h1>
+        </header>
+        <nav>
+          <a href="#">About Us</a>
+          <a href="#">Contact</a>
+          <a href="#">Portfolio</a>
+        </nav>
+      </div>
+      <progress-bar :progress="progress" />
+      <post @progressUpdate="setProgress"/>
+    </main>
+  </div>
+
 </template>
 
+<script>
+  import post from "@/components/post";
+  import progressBar from "@/components/progressBar";
+
+  export default {
+    name: 'App',
+    components: {
+      post,
+      progressBar
+    },
+    data() {
+      return  {
+        progress: 0
+      }
+    },
+    methods: {
+      setProgress(progress) {
+        this.progress = progress
+      }
+    }
+  }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    font-family: 'monospace', sans-serif;
+  }
+  main {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    overflow: hidden;
+  }
+  .navigation {
+    width: 100%;
+    height: 10vh;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background: #000;
+    padding: 30px;
+  }
+  header h1 {
+    color: #fff;
+    font-size: 32px;
+  }
+  nav a {
+    text-transform: uppercase;
+    text-decoration: none;
+    color: #ffffff;
+    font-size: 18px;
+    margin: 0 20px;
+  }
 </style>
---
+++ src/components/post.vue
@@ -0,0 +1,115 @@
+<template>
+  <article>
+    <div class="content">
+      <h1>UEFA Champions</h1>
+      <h2>Real - PSG</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus dicta eum exercitationem minus modi nostrum
+        sit ut? Ad autem beatae blanditiis consequatur dignissimos est facilis fuga, inventore iste magni modi numquam
+        praesentium qui quis quod quos rem repellendus similique voluptate, voluptates! Autem corporis dolore dolores
+        error ex fuga ipsam reprehenderit sapiente suscipit tenetur. Ad aliquam animi consequuntur dolor eaque
+        exercitationem facere id, incidunt ipsum labore maiores natus perferendis quaerat quibusdam tenetur ut,
+        voluptate. At modi, praesentium? Adipisci alias animi assumenda autem dolore eveniet ex id ipsum itaque iure
+        mollitia nobis porro quasi quia quis quo saepe sint, veniam veritatis voluptas.</p>
+      <p>Aliquam aperiam cupiditate distinctio excepturi exercitationem, expedita explicabo facere fuga fugiat fugit
+        ipsam laborum molestias nam officia porro quam qui reiciendis reprehenderit sed soluta tenetur, vel voluptatem?
+        Accusamus deleniti doloremque earum error eum excepturi labore optio quisquam quod sint. Accusantium ad, aliquid
+        aspernatur autem cumque, dicta dolor et excepturi, facere id libero nam nihil odit officia quibusdam quisquam
+        repellat sapiente similique ut veniam. Asperiores ducimus ea eaque earum eius eligendi eos esse explicabo,
+        fugiat inventore mollitia natus perferendis porro possimus quam quas quasi quidem quo repudiandae voluptatibus.
+        Ad, culpa, cumque eius esse fugit iusto natus neque repellendus totam ullam veniam.</p>
+      <p>Adipisci asperiores assumenda atque cum cupiditate eveniet, expedita in ipsa iure maiores, natus necessitatibus
+        nihil odit optio placeat sapiente, velit voluptatum? A, aliquid corporis debitis enim eum laudantium maiores
+        necessitatibus nesciunt optio praesentium quas quia, sit tempora vel veniam? Deleniti deserunt doloremque eos,
+        esse maiores maxime voluptatum. Adipisci assumenda commodi consectetur consequatur cumque dignissimos dolorum
+        eveniet facere hic id laboriosam magnam maiores necessitatibus nisi nulla numquam, perferendis quia quisquam quo
+        quos recusandae reiciendis tempora, ut vel, veritatis voluptas voluptatibus? Ad adipisci aspernatur beatae
+        consectetur dicta eius enim id neque nisi odit provident qui quis quos repellendus, sapiente vel vero
+        voluptates.</p>
+      <p>Alias animi aperiam autem cupiditate ducimus eius est ipsam iusto, magnam neque nisi omnis pariatur, quaerat
+        quam qui quis repudiandae rerum, soluta tempore vero! Amet dignissimos ipsa iste laboriosam neque officiis
+        quaerat quam quidem quos, ratione, repudiandae sequi soluta tempora? Atque cumque et eum eveniet exercitationem
+        molestias perspiciatis veritatis! Accusamus alias aliquid animi, aperiam aspernatur autem blanditiis consequatur
+        culpa distinctio doloremque ducimus eius error eveniet explicabo hic, illo iste iure natus necessitatibus non
+        obcaecati omnis placeat praesentium quam quas qui recusandae reprehenderit sunt totam unde. Atque corporis
+        delectus ducimus ipsam mollitia necessitatibus omnis vitae. Eligendi illo impedit incidunt laudantium
+        tempore.</p>
+      <p>A accusamus accusantium aliquam architecto atque autem dicta distinctio ea enim, eum, ipsam laborum magni
+        maiores mollitia natus non odit pariatur, provident quasi quisquam repellat reprehenderit similique? Alias
+        beatae consequatur culpa cum deleniti dignissimos distinctio ducimus error esse et ex fuga in inventore ipsam
+        laborum maxime nihil nisi odit, pariatur quisquam rem temporibus voluptas. Accusantium aperiam consequatur
+        corporis dicta dolorum ea fugit in incidunt laboriosam, nam nesciunt, odio officia, ratione similique sint
+        soluta voluptatum! Doloribus exercitationem laborum nisi quos ratione! Aliquid eum modi, obcaecati recusandae
+        rem suscipit? Animi culpa id maiores omnis placeat provident quidem, rem. Nobis quibusdam recusandae
+        reiciendis.</p>
+      <p>Neque nihil possimus recusandae. Aliquam, aliquid, aperiam asperiores, dolorem eligendi molestias numquam odio
+        omnis qui rem repudiandae suscipit tempore voluptatibus. Dolorum eveniet, facilis ipsa odio optio praesentium
+        quidem sint totam! Aliquam aspernatur corporis debitis, deleniti dolor dolorem doloremque dolorum error, ex
+        facere illum ipsa ipsam laboriosam molestiae molestias necessitatibus nesciunt nisi numquam omnis perferendis,
+        possimus quibusdam rem reprehenderit repudiandae similique tempore veniam voluptates! Aliquid architecto eius
+        fugiat illum impedit mollitia necessitatibus nesciunt, pariatur, perspiciatis quia quibusdam, reiciendis sunt
+        velit vitae voluptate! Excepturi exercitationem quis quo rem, sunt totam veniam? Dignissimos eaque hic ut!
+        Assumenda beatae commodi est, natus odio sit!</p>
+      <p>Delectus dolorem earum incidunt inventore ipsa iure pariatur perspiciatis quas quos sint. Deserunt doloribus
+        est itaque laboriosam maiores nisi non odio pariatur, porro provident recusandae sapiente sint tempora, totam
+        unde vel voluptate. Accusamus aspernatur atque, beatae excepturi fugiat inventore minus nisi numquam optio quos.
+        Cupiditate enim fugit mollitia nihil quae quo recusandae similique tenetur, vel vero. Aut, ex nihil. Accusamus
+        architecto commodi cupiditate dolor ducimus esse expedita explicabo illum itaque laboriosam, necessitatibus
+        nihil obcaecati officia, reprehenderit tenetur. Architecto consequatur consequuntur culpa dolor eos est facilis
+        fugit modi molestiae natus nemo omnis pariatur provident quam quibusdam saepe unde, vero, voluptas
+        voluptatibus.</p>
+      <p>Ab accusantium aliquid consequuntur corporis cumque debitis deserunt dolor dolore doloremque dolores doloribus
+        est ex hic impedit iure labore libero nemo nobis nulla omnis pariatur quaerat quasi qui quia quisquam ratione
+        rem saepe sed, sequi similique tempore tenetur unde voluptatem! Ea facere incidunt nam voluptatum? Ad beatae
+        distinctio dolores eveniet, excepturi facere facilis fugit inventore iste magnam molestias nisi nostrum
+        obcaecati officia quaerat quam qui quibusdam quidem quod rem rerum sunt velit. Accusamus, aliquam atque beatae
+        consequuntur dolores doloribus eveniet facilis hic illum labore libero minus nisi non numquam officia quas quia
+        quibusdam quidem quis soluta temporibus veniam, veritatis voluptatibus.</p>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "post",
+  mounted() {
+    this.$el.addEventListener('scroll', function () {
+      let scrollTop = this.$el.scrollTop
+      let clientHeight = this.$el.clientHeight
+      let scrollHeight = this.$el.scrollHeight
+
+      let scrollPercent = Math.round((scrollTop) / (scrollHeight - clientHeight) * 100)
+      this.$emit('progressUpdate', scrollPercent)
+    }.bind(this))
+  }
+}
+</script>
+
+<style>
+  article {
+    width: 100%;
+    flex: 1 1 100%;
+    max-height: calc(100vh - 80px);
+    background: aqua;
+    overflow-y: scroll;
+  }
+  article .content {
+    padding: 30px;
+  }
+  article .content h1 {
+    font-size: 32px;
+    font-weight: 900;
+    margin-bottom: 10px;
+  }
+  article .content h2 {
+    font-size: 22px;
+    font-weight: 300;
+    margin-bottom: 30px;
+  }
+  article .content p {
+    color: #313131;
+    font-size: 18px;
+    font-weight: 300;
+    line-height: 1.5;
+    margin-bottom: 15px;
+  }
+</style>
